3d51d036861feaddd25df3249242cea9c6fee9df
Freddyr91 <45013007+Freddyr91@users.noreply.github.com>
2019-04-13 11:28:52 +0200

    Moved Wall and Grass classes into common environment file

--- environment.py
+++ environment.py
@@ -10,3 +10,14 @@ class Wall(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.pos = pos
         self.rect.topleft = pos * TILESIZE
+
+class Grass(pg.sprite.Sprite):
+    def __init__(self, game, pos):
+        self._layer = FLOOR_LAYER
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = choice(game.grass_imgs)
+        self.rect = self.image.get_rect()
+        self.pos = pos
+        self.rect.topleft = pos * TILESIZE
